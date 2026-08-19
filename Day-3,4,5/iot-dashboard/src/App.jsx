134f6bee949adc46b751a461414e2c79531f9cf4
ninadarondekar <ninadarondekar@gmail.com>
2026-08-19 23:30:59 +0530

    Added css to API.jsx

--- Day-3,4,5/iot-dashboard/src/App.jsx
+++ Day-3,4,5/iot-dashboard/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 // import Eventspage from './Components/Eventspage'
 import API from './Components/API'
+import './App.css'
 
 const App = () => {
   return (
--- Day-3,4,5/iot-dashboard/src/Components/API.jsx
+++ Day-3,4,5/iot-dashboard/src/Components/API.jsx
@@ -1,46 +1,53 @@
-import React from 'react'
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const API = () => {
-            /* useEffect(()=>{
 
-        },[]) */
+    const [data, setData] = useState([])
 
-        const[data, setData] = useState([])
+    useEffect(() => {
 
-        useEffect(() =>{
-            const getUsers = async () =>{
-                try {
-                    const response = await axios.get('https://jsonplaceholder.typicode.com/users');
-                    const data = response.data;
-                    setData(data)
-                    console.log(response.data);
-                } catch (error) {
-                    console.log("API is not working!", error);
-
-                }
-            };
-            getUsers();
-        },[])
-  return (
-    <>
-        <h1>Fetching Users</h1>
-    {/* JSX architecture */}
-        {
-            data.map((item) =>{
-                return(
-                    <div key={item.id}>
-                        <li style={{color:'blue'}}>{item.username}</li>
-                        <p style={{color:'red'}}>{item.email}</p>
-                    </div>
+        const getUsers = async () => {
+            try {
+                const response = await axios.get(
+                    'https://jsonplaceholder.typicode.com/users'
                 )
-            })
+
+                setData(response.data)
+                console.log(response.data)
+
+            } catch (error) {
+                console.log("API is not working!", error)
+            }
         }
 
+        getUsers()
 
-    </>
-  )
+    }, [])
+
+    return (
+        <>
+            <h1>Fetching Users</h1>
+
+            {data.map((item) => {
+                return (
+                    <div className="user-card" key={item.id}>
+                        <p className="name">
+                            Name: {item.name}
+                        </p>
+
+                        <p className="username">
+                            Username: {item.username}
+                        </p>
+
+                        <p className="email">
+                            Email: {item.email}
+                        </p>
+                    </div>
+                )
+            })}
+        </>
+    )
 }
 
 export default API
